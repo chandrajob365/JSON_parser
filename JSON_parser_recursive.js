0be965169dadf935e6e5488813556fb76e25d11c
Manish <chandrajob365@gmail.com>
2017-04-21 02:40:20 +0530

    REgex in boolparser and fix for negative condition in Array parser

--- JSON_parser_recursive.js
+++ JSON_parser_recursive.js
@@ -2,149 +2,168 @@ const fs = require('fs')
 const example = fs.readFileSync("./example.json").toString()
 var numRegex = /^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/
 
-var nullParser = function(str){
-  str = spaceParser(str)
-  return str.slice(0,4)=='null' ? [null ,str.slice(4)] : null
+var nullParser = function(input){
+  input = spaceParser(input)
+  return input.slice(0,4)=='null' ? [null ,input.slice(4)] : null
 }
 
-var booleanParser = function(str){
-  str = spaceParser(str)
-  return str.slice(0,4)=='true' ? [true , str.slice(4)] :
-                          (str.slice(0,5) == 'false' ?
-                                                    [false ,str.slice(5)] :
-                                                    null)
+var booleanParser = function(input){
+  input = spaceParser(input)
+  var match = /(^true|^false)/.exec(input)
+  return match!=null ?  [match[0],input.slice(match[0].length)] : null
 }
 
-var commaParser = function(str){
-  str = spaceParser(str)
-  return str[0]==',' ? [',' , str.slice(1)] : null
+var commaParser = function(input){
+  input = spaceParser(input)
+  return input[0]==',' ? [',' , input.slice(1)] : null
 }
 
-var spaceParser = function(str){
-    return str.match(/\s*/) ? str.replace(/\s*/ , '') : str
+var spaceParser = function(input){
+    return input.match(/\s*/) ? input.replace(/\s*/ , '') : input
 }
 
-var stringParser = function(str){
-  str = spaceParser(str)
-  if(str[0]=='"'){
-    str = str.slice(1)
-    var end_index = str.indexOf('"')
-    if(str.indexOf('\\')>0 && str.indexOf('\\')<end_index)
-      str = str.replace('\\' , '')
-    return [str.slice(0,end_index) , str.slice(end_index+1, str.length)]
+var stringParser = function(input){
+  input = spaceParser(input)
+  if(input[0]=='"'){
+    input = input.slice(1)
+    var end_index = input.indexOf('"')
+    if(input.indexOf('\\')>0 && input.indexOf('\\')<end_index)
+      input = input.replace('\\' , '')
+    return [input.slice(0,end_index) , input.slice(end_index+1, input.length)]
   }
   return null
+}
+
+/* Array Validity Checker */
+var chkValidArray = function(res){
+  var input = spaceParser(res[1])
+  var hasComma = commaParser(input)
+  if(input==='') return false  // to check for input of type [12 , 13 , or [12 , 13
+  if(hasComma){
+    if(!parser(spaceParser(hasComma[1]))) return false  // to check for input of type [12 , 14 , ]
+  }
+  if(res[0]!==',' && !hasComma && input[0]!==']') return false // to check for input of type [12 , 13 14]
+  else return true
 }
 
 /* Helper function for arrayParser */
-var helperArrayParser = function(str,temp){
-  if(str.length>0 && str[0]!=']'){
-    str = spaceParser(str)
-    var res = parser(str)
-    if(res==null) return null
-    if (res[0] != ",") temp.push(res[0])
-    str = res[1]
-    str = spaceParser(res[1])
-    var test
-    if(test=commaParser(str)){
-      if(!parser(test[1])) return null
+var helperArrayParser = function(input,outArr){
+  if(input.length>0 && input[0]!=']'){
+    var res = parser(spaceParser(input))
+    if(res===null) return null
+    if (res[0] != ",") outArr.push(res[0])
+    input = spaceParser(res[1])
+    if(!chkValidArray(res)) return null;
+    return helperArrayParser(input,outArr)
     }
-    if(str=='') return null
-    return helperArrayParser(str,temp)
-    }
-    return [spaceParser(str) , temp]
+    return [spaceParser(input) , outArr]
 }
 
 
-var arrayParser = function(str){
-  str = spaceParser(str)
-  if(str[0]=='['){
-    str = spaceParser(str.slice(1))
-    var temp = []
-    var res = helperArrayParser(str,temp)
-    if(res==null) return null
-    else [str,temp] = res
-    return [temp , str.slice(1)]
+var arrayParser = function(input){
+  input = spaceParser(input)
+  if(input[0]=='['){
+    input = spaceParser(input.slice(1))
+    var outArr = []
+    var res = helperArrayParser(input,outArr)
+    if(res===null) return null
+    else [input,outArr] = res
+    return [outArr , input.slice(1)]
   }
   return null
 }
 
 
-var numberParser = function(str){
-  str = spaceParser(str)
+var numberParser = function(input){
+  input = spaceParser(input)
   var res
-  if(res = numRegex.exec(str)){
+  if(res = numRegex.exec(input)){
     res_len = res.index+res[0].length
     var num = parseFloat(res[0])
-    return [num , str.slice(res_len)]
+    return [num , input.slice(res_len)]
   }
   return null
 }
 
-var keyParser = function(str){
-  return stringParser(spaceParser(str))
+var keyParser = function(input){
+  return stringParser(spaceParser(input))
 }
 
-var valueParser = function(str){
-  return parser(spaceParser(str))
+var valueParser = function(input){
+  return parser(spaceParser(input))
 }
 
-var colonParser = function(str){
-  return str[0] == ':' ? str.slice(1) : str
+var colonParser = function(input){
+  return input[0] == ':' ? input.slice(1) : input
 }
 
 
 /* Helper function for objectParser */
-var helperObjectParser = function(str,outArrObj){
-  if(str.length>0 && str[0]!='}'){
-    var key = keyParser(str)
+var helperObjectParser = function(input,outArrObj){
+  if(input.length>0 && input[0]!='}'){
+    var key = keyParser(input)
+    console.log("key--->",key);
     if(key==null) return null
-    str = colonParser(spaceParser(key[1]))
-    value = valueParser(str)
+    input = colonParser(spaceParser(key[1]))
+    value = valueParser(input)
+    console.log("input--->",input);
+    console.log("value--->",value);
     if(value==null) return null
-    str = spaceParser(value[1])
+    input = spaceParser(value[1])
+    console.log("spaceParser(value[1])--->",input[0]);
     outArrObj[key[0]] = value[0]
-    if(str[0]==','){
-      str = str.slice(1)
-      if(keyParser(str)==null) return null
+    if(input[0]==','){
+      input = input.slice(1)
+      if(keyParser(input)==null) return null
+    }/*else if(input[0]!==','){
+      input = input.slice(1)
+      console.log("Inside input[0]!==','  input--->",input);
+      if(keyParser(input)!==null && input[0]!=='}') return null
+    }*/
+    /*if(input[0]!==',' && input[0]!=='}'){
+      console.log("Inside input[0]!==',' before slicing input--->",input[0]);
+      //input = input.slice(1)
+      console.log("Inside input[0]!==','  input--->",input);
+      if(keyParser(input.slice(1))!==null) return null
+    }*/
+      return helperObjectParser(input,outArrObj)
     }
-      return helperObjectParser(str,outArrObj)
-    }
-    return [str, outArrObj]
+    return [input, outArrObj]
 }
 
-var objectParser = function(str){
-  str = spaceParser(str)
+var objectParser = function(input){
+  console.log("input--->",input);
+  input = spaceParser(input)
   var outArrObj={}
-  if(str[0]=='{'){
-    str = spaceParser(str.slice(1))
-    var res = helperObjectParser(str,outArrObj)
+  if(input[0]=='{'){
+    input = spaceParser(input.slice(1))
+    var res = helperObjectParser(input,outArrObj)
     if(!res) return null
-    else [str,outArrObj] = res
-    if(outArrObj!=undefined) return [outArrObj , str.slice(1)]
+    else [input,outArrObj] = res
+    if(outArrObj!=undefined) return [outArrObj , input.slice(1)]
   }
   return null
 }
 
-function parser(str){
+function parser(input){
   var res
-if(res=nullParser(str))
+if(res=nullParser(input))
   return res
-if(res = booleanParser(str.trim()))
+if(res = booleanParser(input))
   return res
-if(res = stringParser(str.trim()))
+if(res = stringParser(input))
   return res
-if(res= arrayParser(str.trim()))
+if(res= arrayParser(input))
   return res
-if(res = commaParser(str))
+if(res = commaParser(input))
   return res
-if(res = numberParser(str))
+if(res = numberParser(input))
    return res
-if(res = objectParser(str))
+if(res = objectParser(input))
    return res
-if(str !== '')
+if(input !== '')
   return null
 }
 
 var parsedOutput = parser(example)
- parsedOutput!= null ? (spaceParser(parsedOutput[1])=='' ? console.log(JSON.stringify(parsedOutput[0], null, 4)) : console.log("Invalid JSON")): console.log("Invalid JSON")
+ parsedOutput!= null ? (spaceParser(parsedOutput[1])=='' ? console.log(JSON.stringify(parsedOutput[0], null, 2)) : console.log("Invalid JSON")): console.log("Invalid JSON")
